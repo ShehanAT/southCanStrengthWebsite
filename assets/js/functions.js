a90e1e1d7c069dadba3a28f276b8bcbbdb4300d5
Shehan Atukorala <Shehan@Shehans-MacBook-Pro.local>
2019-08-06 17:07:54 -0400

    made about section responsive with responsive img

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,15 @@
 function showExitButton(){
     if(document.getElementById("nav-check").checked){
+        document.getElementById("about").style.display = "block";
+        document.getElementById("services").style.display = "block";
+        document.getElementById("footer").style.display = "block";
+        document.getElementById("contact").style.display = "block";
         document.getElementById("nav-check__label").classList.remove("dropdown-active");
     }else{
+        document.getElementById("about").style.display = "none";
+        document.getElementById("services").style.display = "none";
+        document.getElementById("footer").style.display = "none";
+        document.getElementById("contact").style.display = "none";
         document.getElementById("nav-check__label").classList.add("dropdown-active");
     }
     
