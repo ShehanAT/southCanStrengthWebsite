c25500b712feec9eb4ee0b9728e249243ac3855b
Shehan Atukorala <Shehan@Shehans-MacBook-Pro.local>
2019-08-07 19:59:59 -0400

    added docs folder

---
+++ docs/assets/js/functions.js
@@ -0,0 +1,42 @@
+function showExitButton(){
+    if(document.getElementById("nav-check").checked){
+        document.getElementById("about").style.display = "block";
+        document.getElementById("services").style.display = "block";
+        document.getElementById("footer").style.display = "block";
+        document.getElementById("contact").style.display = "block";
+        document.getElementById("nav-links").style.height = "0px";
+        document.getElementById("nav-check__label").classList.remove("dropdown-active");
+    }else{
+        document.getElementById("about").style.display = "none";
+        document.getElementById("services").style.display = "none";
+        document.getElementById("footer").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        document.getElementById("nav-links").style.height = "calc(100vh - 50px)";
+        document.getElementById("nav-check__label").classList.add("dropdown-active");
+    }
+    
+}
+function showContactExitButton(){
+    if(document.getElementById("nav-check").checked){
+        document.getElementById("enquiry").style.display = "block";
+        document.getElementById("nav-links").style.height = "0px";
+        
+        //enables css animation on hamburger icon 
+        document.getElementById("nav-check__label").classList.remove("dropdown-active");
+    }else{
+        document.getElementById("enquiry").style.display = "none";
+        document.getElementById("nav-links").style.height = "calc(100vh - 50px)";
+        //enables css animation on hamburger icon 
+        document.getElementById("nav-check__label").classList.add("dropdown-active");
+    } 
+}
+
+function showSection(){
+    document.getElementById("about").style.display = "block";
+    document.getElementById("services").style.display = "block";
+    document.getElementById("footer").style.display = "block";
+    document.getElementById("contact").style.display = "block";
+    document.getElementById("nav-links").style.height = "0px";
+    document.getElementById("nav-check").checked = false;
+    document.getElementById("nav-check__label").classList.remove("dropdown-active");
+}
